8c3fb6c2a850b8b0a3b3562fe88be3afe112ed4e
tonbar <sara.rey.manchester@gmail.com>
2020-02-05 12:26:13 +0000

    tests added

--- shire_querier.py
+++ shire_querier.py
@@ -5,6 +5,29 @@ from config import path_to_java
 from config import encoding
 
 
+def query_shire(worksheet_id, test_db_or_not):
+    return subprocess.check_output([path_to_java, '-jar', 'query_shire.jar', worksheet_id, test_db_or_not])
+
+
+def query_shire_to_file(worksheet_id, test_db_or_not, output_filename):
+    return subprocess.check_output([path_to_java, '-jar', 'query_shire.jar', worksheet_id, test_db_or_not,
+                                    output_filename])
+
+
+def load_shire_file(file_name):
+    try:
+        with open(file_name) as f:
+            shire_data = json.load(f)
+    except IOError:
+        raise FileNotFoundError(f"Unable to load data from file {file_name}. File not found.")
+    return shire_data
+
+
+def convert_bin_to_dictionary(bin_data):
+    s = bin_data.decode(encoding)
+    return json.loads(s)
+
+
 def get_worksheet_id(data):
     return data.get('worksheet')
 
@@ -19,9 +42,9 @@ def get_sample_data(data):
 
 def main():
     # Call java jar file to query shire database
-    o = subprocess.check_output([path_to_java, '-jar', 'query_shire.jar', '20-00', 'true'])
-    s = o.decode(encoding)
-    d = json.loads(s)
+    o = query_shire("20-00", "true")
+    d = convert_bin_to_dictionary(o)
+    print(o)
 
 
 if __name__ == '__main__':
--- test_shire_querier.py
+++ test_shire_querier.py
@@ -1,11 +1,45 @@
 import unittest
+import os
+import getpass
+from shire_querier import *
+
 
 class TestShire(unittest.TestCase):
     def setUp(self) -> None:
-        p = "p"
+        test_raw_data = query_shire("20-00", "true")
+        self.test_data = convert_bin_to_dictionary(test_raw_data)
 
     def test_worksheet_id(self):
-        self.assertEqual(shire_querier., "20-00")
+        self.assertEqual(get_worksheet_id(self.test_data), "20-00")
+
+    def test_user(self):
+        self.assertEqual(get_user(self.test_data), getpass.getuser())
+
+    def test_samples(self):
+        self.assertEqual(get_sample_data(self.test_data),
+                         [{"labNo": "19M1", "position": "1", "worksheet": "20-00", "test": "CRM Panel",
+                           "updatedDate": "2020-01-20 00:00:00", "sex": "m", "referralReason": "Colorectal",
+                           "firstName": "David", "comments": "FOCUS4"},
+                          {"labNo": "19M2", "position": "2", "worksheet": "20-00", "test": "CRM Panel",
+                           "updatedDate": "2020-01-25 00:00:00", "sex": "m", "referralReason": "Melanoma",
+                           "firstName": "Peter", "comments": None},
+                          {"labNo": "19M3", "position": "3", "worksheet": "20-00", "test": "CRM Panel",
+                           "updatedDate": "2019-05-14 00:00:00", "sex": "f", "referralReason": "Colorectal",
+                           "firstName": "Susan", "comments": "FOCUS4"},
+                          {"labNo": "19M4", "position": "4","worksheet": "20-00", "test": "CRM Panel",
+                           "updatedDate": "2020-01-25 00:00:00", "sex": "m", "referralReason": "Melanoma",
+                           "firstName": "Thomas", "comments": None}])
+
+    def test_file_generation(self):
+        query_shire_to_file("20-00", "true", "test.json")
+        assert os.path.exists("test.json")
+        os.remove("test.json")
+
+    def test_file_load(self):
+        query_shire_to_file("20-00", "true", "test.json")
+        data_from_file = load_shire_file("test.json")
+        assert type(data_from_file) is dict and data_from_file
+        os.remove("test.json")
 
 
 if __name__ == '__main__':
